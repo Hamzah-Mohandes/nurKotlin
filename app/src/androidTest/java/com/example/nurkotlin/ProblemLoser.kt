25a41d69cf3892081c12eb4eb63cb425a49d4735
Hamzah <rootmoodrm@gmail.com>
2025-03-25 22:16:40 +0100

    neue

--- app/src/androidTest/java/com/example/nurkotlin/ProblemLoser.kt
+++ app/src/androidTest/java/com/example/nurkotlin/ProblemLoser.kt
@@ -4,40 +4,17 @@ class ProblemLoser {
 
 }
 
-data class  User(val name: String, val age: Int)
-
-
-
-// Enums
-
-enum class Color {
-    RED,
-    GREEN,
-    BLUE
-}
 
 fun main() {
-    val color = Color.GREEN
-    when (color) {
-        Color.RED -> println("The color is red")
-        Color.GREEN -> println("The color is green")
-        Color.BLUE -> println("The color is blue")
+     println("du erwachst in einem dunklen Raum , Was tust du?")
+    println("1. Umsehen")
+    println("2. Rufen")
+
+    val antwort = readln()
+
+    when(antwort){
+     "1" -> println("du sihst eine schwache Lampe und eine Tür.")
+     "2" -> println("Niemand antwortet. Es ist still")
+     else -> println("Du tust nichts.")
     }
-
-    val shape: Shape = Shape.Circle(5)
-    when (shape) {
-        is Shape.Circle -> println("The shape is a circle with radius ${shape.radius}")
-        is Shape.Square -> println("The shape is a square with side length ${shape.sideLength}")
-        is Shape.Rectangle -> println("The shape is a rectangle with width ${shape.width} and height ${shape.height}")
-    }
-}
-
-
-
-
-// Sealed
-sealed class Shape {
-    class Circle(val radius: Int) : Shape()
-    class Square(val sideLength: Int) : Shape()
-    class Rectangle(val width: Int, val height: Int) : Shape()
 }
